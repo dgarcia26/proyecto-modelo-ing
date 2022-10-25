e7090c45613c28b8e296edcc5957c35fd1d620c7
Darwin G <dgarcia26@cuc.edu.co>
2022-10-25 17:47:06 -0500

    Preparando proyecto

---
+++ src/components/Prueba.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const prueba = () => {
+  return (
+    <div>
+        <div className="container">
+          <div className="row">
+            <div className="col-8">
+              <h1>titulo</h1>
+            </div>
+            <div className="col-4">
+              <form action="submin">
+                <input type="text" />Nombre
+              </form>
+            </div>
+          </div>
+
+
+        </div>
+        
+        aqui va el formulario
+    </div>
+  )
+}
+
+export default prueba
