eafe2b561269e1126c32b777822c670c421f9411
Darwin Garcia <soporte.darwin.garcia@gmail.com>
2022-11-28 18:44:21 -0500

    eslint

--- src/components/Simulacion.jsx
+++ src/components/Simulacion.jsx
@@ -21,6 +21,7 @@ const Simulacion = () => {
         <div className="col-1">
         </div>
         <div className="col-10">
+        {/*eslint-disable-next-line*/}
         <iframe src="https://main--marvelous-blini-4a1a67.netlify.app/" className='container text-center' frameborder="100"height="900" width="900"/>         
         </div>
         <div className="col-1"></div>
